ff9869322d9c54e6e7b6180f0dfde003c29d2303
Terry Crowley <tcrowley@alum.mit.edu>
2017-07-18 09:15:21 -0700

    Cleanup public - now part of client build.

--- tools/archive.sh
+++ tools/archive.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-SRC=./js-share
-DST=${SRC}.deploy
+SRCSERVER=./js-share-server
+SRCCLIENT=./js-share
+DST=${SRCSERVER}.deploy
 
-if [ ! -e ${SRC} ]; then
-	echo Run archive script from parent directory of ${SRC}
+if [ ! -e ${SRCSERVER} ]; then
+	echo Run archive script from parent directory of ${SRCSERVER}
 	exit 1
 fi
 
@@ -14,24 +15,24 @@ rm -rf ${DST}
 mkdir ${DST}
 mkdir ${DST}/node_modules
 
-cp ${SRC}/package.deploy.json ${DST}/package.json
-cp ${SRC}/server.js ${DST}
-cp -R ${SRC}/dist ${DST}
-cp -R ${SRC}/clientdist ${DST}
-cp -R ${SRC}/public ${DST}
-cp -R ${SRC}/pages ${DST}
-cp -R ${SRC}/state ${DST}
+cp ${SRCSERVER}/package.deploy.json ${DST}/package.json
+cp ${SRCSERVER}/server.js ${DST}
+cp -R ${SRCSERVER}/dist ${DST}
+cp -R ${SRCCLIENT}/clientdist ${DST}
+cp -R ${SRCCLIENT}/public ${DST}
+cp -R ${SRCSERVER}/pages ${DST}
+cp -R ${SRCSERVER}/state ${DST}
 rm ${DST}/state/*
-cp -R ${SRC}/node_modules/express ${DST}/node_modules
-cp -R ${SRC}/node_modules/express-session ${DST}/node_modules
-cp -R ${SRC}/node_modules/passport ${DST}/node_modules
-cp -R ${SRC}/node_modules/passport-facebook ${DST}/node_modules
-cp -R ${SRC}/node_modules/passport-local ${DST}/node_modules
-cp -R ${SRC}/node_modules/body-parser ${DST}/node_modules
-cp -R ${SRC}/node_modules/cookie-parser ${DST}/node_modules
-cp -R ${SRC}/node_modules/connect-flash ${DST}/node_modules
-cp -R ${SRC}/node_modules/bcryptjs ${DST}/node_modules
-cp -R ${SRC}/node_modules/@terrencecrowley ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/express ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/express-session ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/passport ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/passport-facebook ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/passport-local ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/body-parser ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/cookie-parser ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/connect-flash ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/bcryptjs ${DST}/node_modules
+cp -R ${SRCSERVER}/node_modules/@terrencecrowley ${DST}/node_modules
 cd ${DST}
 zip -q -r archive.zip .
 mv archive.zip ..
